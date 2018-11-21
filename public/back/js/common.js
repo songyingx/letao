315dfde201530eb76db8dea2bb84c8050cf57322
songying <songying@qq.com>
2018-11-21 21:07:28 +0800

    公共功能完成（侧边栏切换，二级菜单切换，退出功能)

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,17 +1,50 @@
-$(function() {
-   /*
-    // 开启进度条
-    NProgress.start() ;
-    // 结束进度条
-    NProgress.done();
-    */
+$(function () {
+    /*
+     // 开启进度条
+     NProgress.start() ;
+     // 结束进度条
+     NProgress.done();
+     */
 
 
-    $(document).ajaxStart(function() {
+    $(document).ajaxStart(function () {
         NProgress.start();
     })
 
-    $(document).ajaxStop(function() {
+    $(document).ajaxStop(function () {
         NProgress.done();
+    })
+
+
+    // 公用功能
+    // 1左侧二级切换功能
+    $('#category').click(function () {
+        $(this).next().stop().slideToggle();
+    })
+
+    // 2.左边菜单切换功能
+    $('.lt_topbar .icon_left').click(function () {
+        $('.lt_aside').toggleClass('hidemenu');
+        $('.lt_topbar').toggleClass('hidemenu');
+        $('.lt_main').toggleClass('hidemenu');
+    })
+
+    // 3.公共退出功能
+    $('.lt_topbar .icon_right').click(function () {
+        $('#logoutModal').modal('show');
+    });
+    $('#logoutBtn').click(function () {
+        // 调用接口，
+        $.ajax({
+            type: "get",
+            url: "/employee/employeeLogout",
+            dataType: "json",
+            success: function (info) {
+                console.log(info);
+                if (info.success) {
+                    location.href = "login.html";
+                }
+            }
+        })
     })
 })

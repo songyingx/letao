09c605d7fdcd03d09b9a22b7db20b09e7f7b5a61
songying <songying@qq.com>
2018-11-22 21:56:40 +0800

    二级分类页面完成和添加分类一半完成

---
+++ public/back/js/second.js
@@ -0,0 +1,61 @@
+$(function () {
+    var currentPage = 1;
+    var pageSize = 5;
+
+    render();
+
+    function render() {
+        $.ajax({
+            type: "get",
+            url: "/category/querySecondCategoryPaging",
+            data: {
+                page: currentPage,
+                pageSize: pageSize
+            },
+            dataType: "json",
+            success: function (info) {
+                console.log(info);
+                var htmlStr = template('secondTpl', info)
+                $('tbody').html(htmlStr);
+
+                $('#paginator').bootstrapPaginator({
+                    bootstrapMajorVersion: 3,
+                    currentPage: info.page,
+                    totalPages: Math.ceil(info.total / info.size),
+                    onPageClicked: function (a, b, c, page) {
+                        currentPage = page;
+                        render();
+                    }
+                })
+            }
+        })
+    }
+
+
+    $('#addBtn').click(function () {
+        $('#addModal').modal("show");
+
+        $.ajax({
+            type: "get",
+            url: "/category/queryTopCategoryPaging",
+            data: {
+                page: 1,
+                pageSize: 100
+            },
+            dataType: "json",
+            success: function (info) {
+                console.log(info);
+                var htmlStr = template("dropdownTpl", info);
+                $('.dropdown-menu').html(htmlStr);
+            }
+        })
+    })
+
+
+    $('.dropdown-menu').on("click", "a", function () {
+        var txt = $(this).text();
+        $('#dropdownText').text(txt);
+
+    })
+
+})

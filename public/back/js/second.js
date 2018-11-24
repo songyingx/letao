3e776a20932688e858ccc99c51adeeef066b5520
songying <songying@qq.com>
2018-11-24 14:40:27 +0800

    二级分类添加完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -114,21 +114,25 @@ $(function () {
 
     });
 
-    $("#form").on("success.form.bv",function( e ) {
+    $("#form").on("success.form.bv", function (e) {
         e.preventDefault();
 
         $.ajax({
-            type:"post",
+            type: "post",
             url: "/category/addSecondCategory",
-            data:$('#form').serialize(),
+            data: $('#form').serialize(),
             dataType: "json",
-            success:function( info ){
+            success: function (info) {
                 console.log(info);
+                if (info.success) {
+                    $('#addModal').modal("hide");
+                    currentPage = 1;
+                    render();
+                    $('#form').data("bootstrapValidator").resetForm(true);
+                    $('#dropdownText').text("请选择一级分类");
+                    $('#imgBox img').attr("src", "./images/none.png");
+                }
 
-                $('#addModal').modal("hide");
-                currentPage = 1;
-                render();
-                $('#form').data("bootstrapValidator").resetForm(true);
             }
         })
     })
